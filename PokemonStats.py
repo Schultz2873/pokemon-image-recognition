c3f2217159f6c7887688514a5eee45ef88b1be92
steve <b2273469@ben.edu>
2018-09-25 22:13:22 -0500

    Formatting results from CSV File

--- PokemonStats.py
+++ PokemonStats.py
@@ -1,7 +1,7 @@
 import csv
 from Pokemon import Pokemon
 
-with open('pokemon-stats.csv') as file:
+with open('../cleaned/pokemon-stats-clean.csv') as file:
     pokemon_list = []
 
     reader = csv.reader(file)
@@ -15,6 +15,13 @@ with open('pokemon-stats.csv') as file:
                 pokemon_list.append(pokemon)
 
         count = count + 1
+    #    Output of list
+    print()
+    print("%s %6s %13s %10s %10s %13s %8s %8s %16s %18s %7s %11s %11s %9s %19s %9s %9s %13s" % ("Number", "Name", "Type1", "Type2", "Total", "Hit Points", "Attack", "Defense", "Special Attack", "Special Defense", "Speed", "Generation", "Legendary", "Color", "Mega Evolution", "Height", "Weight", "Body Type"))
+    print()
+    for pokemon in pokemon_list:
+        print(f'{pokemon.number:8} {pokemon.name:12} {pokemon.type1:10} {pokemon.type2:10} {pokemon.total:8} {pokemon.hp:12} {pokemon.attack:7} {pokemon.defense:10} {pokemon.special_attack:16} {pokemon.special_defense:17} {pokemon.speed:7} {pokemon.generation:12} {pokemon.isLegendary:12} {pokemon.color:10} {pokemon.has_mega_evolution:19}{pokemon.height:9} {pokemon.weight:9} {pokemon.body_style:7}')
+
 
 
 
